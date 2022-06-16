930f991bf47f02f216ed8e68e57a4bce6d200a9b
Aakash kumar raj <ar0829@srmist.edu.in>
2022-06-16 19:07:25 +0530

    update

---
+++ socialbook/sciript.js
@@ -0,0 +1,5 @@
+var settingsmenu = document.querySelector(".settings-menu");
+
+function settingsMenuToggle(){
+
+}
---
+++ socialbook/socialbook_img/script.js
@@ -0,0 +1,5 @@
+var settingsmenu = document.querySelector(".settings-menu");
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
+}
